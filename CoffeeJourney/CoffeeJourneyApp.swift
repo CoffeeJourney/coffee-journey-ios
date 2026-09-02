55d37dc6e398c1ee97b72659ed23e440dbfca8c8
Issa Jaguraga <issa.jaguraga9@gmail.com>
2026-09-02 15:31:33 +0200

    chore: init project

---
+++ CoffeeJourney/CoffeeJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeJourneyApp.swift
+//  CoffeeJourney
+//
+//  Created by Issa on 02/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
